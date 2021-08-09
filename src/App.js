8c9e55b591b9ddadae31c53ba31ae0e9a2c03b9d
Dimitra Weinstein <drenee.weinstein@gmail.com>
2021-08-09 14:25:15 -0700

    created file and component structure, basic render of a page compiled

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  // Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
+import Header from '../src/Header.js';
+import Home from '../src/navComponents/Home.js';
+import AboutMe from '../src/navComponents/AboutMe.js';
+import ConnectWithMe from '../src/navComponents/ConnectWithMe.js';
+import TheCode from '../src/navComponents/TheCode.js';
+import Resume from '../src/navComponents/Resume.js';
+import SocialMedia from '../src/navComponents/SocialMedia';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div >
+        <Router>
+          <Header />
+          <Switch>
+            <Route exact path = '/' render = {(routerProps) =>
+              <Home {...routerProps} />}
+            />
+            <Route exact path = '/aboutme' render = {(routerProps) =>
+              <AboutMe {...routerProps} />}
+            />
+            <Route exact path ='/thecode' render = {(routerProps) =>
+            <TheCode {...routerProps} />} 
+            />
+            <Route exact path='/connect' render={(routerProps) =>
+              <ConnectWithMe {...routerProps} />}
+            />
+            <Route exact path='/resume' render={(routerProps) =>
+              <Resume {...routerProps} />}
+            />
+            <Route exact path='/socialmedia' render={(routerProps) =>
+              <SocialMedia {...routerProps} />}
+              />
+          </Switch>
+        </Router>
+      </div>
+    )
+  }
 }
-
-export default App;
+;
---
+++ src/navComponents/TheCode.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class TheCode extends Component {
+    render() {
+        return (
+            <div>
+                Here's My Code!
+            </div>
+        )
+    }
+ }
