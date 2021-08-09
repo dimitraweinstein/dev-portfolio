65eef61a13032eda99a589b56b0d0a5f65dd8b9b
Dimitra Weinstein <drenee.weinstein@gmail.com>
2021-08-09 14:57:47 -0700

    created header links

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 // import { Link } from 'react-router-dom';
 
 export default class Header extends Component {
     render() {
         return (
-            <div>
+            <div className = 'header-div'>
                 Header
+                <Link to='/'>Home</Link>
+                <Link to='/aboutme'>About Me</Link>
+                <Link to='/thecode'>The Code</Link>
+                <Link to='/connect'>Connect With Me</Link>
+                <Link to='/resume'>Resume</Link>
+                <Link to = '/socialmedia'>Social Media</Link>
             </div>
         )
     }
