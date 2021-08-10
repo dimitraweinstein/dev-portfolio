9b922fc93432d6b20902975a84db2974a4b43b20
Dimitra Weinstein <drenee.weinstein@gmail.com>
2021-08-09 21:14:24 -0700

    added header logo, made other css adjustments, and made beginning nav link adjustments

--- src/Header.js
+++ src/Header.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
 import { NavLink, Route } from 'react-router-dom';
+import headerDevLogo from '../src/images/Dev_Logo_W2.png';
 
 export default class Header extends Component {
     render() {
         return (
             <div className = 'header-div'>
                 <Route>
-                    <NavLink to='/'>Home</NavLink>
-                    <NavLink to='/thecode'>The Code</NavLink>
-                    <NavLink to='/connect'>Connect With Me</NavLink>
-                    <NavLink to='/resume'>Resume</NavLink>
-                    <NavLink to='/socialmedia'>Social Media</NavLink>
+                    <img className="header-logo" alt="header logo" src={headerDevLogo} />
+                    <div className="nav-links">
+                        <NavLink to='/'>Home</NavLink>
+                        <NavLink to='/thecode'>The Code</NavLink>
+                        <NavLink to='/connect'>Connect With Me</NavLink>
+                        <NavLink to='/resume'>Resume</NavLink>
+                        <NavLink to='/socialmedia'>Social Media</NavLink>
+                    </div>
                 </Route>
             </div>
         )
